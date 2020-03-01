c7118e9cc04ee4f8ed02a1da7a3aceacfc71f976
Maxim Morev <maxmorev@gmail.com>
2020-03-01 19:00:59 +0300

    fix(*): refactoring of email templates

--- src/main/java/ru/maxmorev/awsses/api/controller/SendTemplateController.java
+++ src/main/java/ru/maxmorev/awsses/api/controller/SendTemplateController.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.simpleemail.model.SendTemplatedEmailRequest;
 import com.amazonaws.services.simpleemail.model.SendTemplatedEmailResult;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
